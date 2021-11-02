af9edb0d42ea20003c149ef8a72f76725ad1aae0
nurbek717 <nurbektukhtamishev@gmail.com>
2021-11-02 18:27:34 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var isName = "Nurbek";
+var isFurstName = "Tukhtamishev";
+var isAge = "20";
+var isEmail = "nurbektukhtamishev@gmail.com";
+var isLoginPasword = "*************";
+var isJins = "Erkak";
+var isPasport = "Nurbek";
+var isState = "Uzbekistan";
+var isCtye = "Tahkent";
+var isHome = "Toshkent shahar 17-uy";
+
+
+
+// prompt
+
+var isName = prompt( "Ismingiz");
+var isFurstName = prompt("familyangiz");
+var isAge = prompt("yoshingiz");
+var isEmail = prompt("Email");
+var isLoginPasword = prompt("Pasword");
+var isJins = prompt("jinsingiz");
+var isPasport = prompt("pasport serya raqamigiz");
+var isState = prompt("Davlatingiz");
+var isCtye = prompt("yashash shahringiz");
+var isHome = prompt("uy manzilingiz");
+
+ // info
+
+var info = 
+ "Ismingiz:" + " "  + isName + "\n" +
+ "Fmilyangis:" + " " + isFurstName + "\n" +
+ "Yoshingiz:" + " " + isAge +  "\n" +
+ "Email:" + " " + isEmail + "\n" +
+ "Login pasword:" + " " + isLoginPasword + "\n" +
+ "Jingizni kiriting:" + " " + isJins + "\n" +
+ "pasport seryangizni kiriting:" + " " + isPasport + "\n" +
+ "Davlatingizni kiriting :" + " " + isState+ "\n" +
+ "Shaharingizni kiriting:" + " " + isCtye + "\n" +
+ "Uyingiznu kiriting:" + " " + isHome + "\n" 
+ ;
+
+console.log(info)
+alert(info)
+  
